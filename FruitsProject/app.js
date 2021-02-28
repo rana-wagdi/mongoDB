037c0d25cbe58979e1e1ec5351f6f219f3e02a92
rana2020701 <ranawagdi4@gmail.com>
2021-02-28 04:00:35 +0200

    add documents

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -26,4 +26,22 @@ client.connect(function(err){
     client.close();
 });
 
+//insert Document
+
+const insertDocument = function(db, callback){
+    //Get the documents collection
+    const collection = db.collection('fruits');
+    //Insert some documents
+    collection.insertMany([
+        {a : 1},
+        {a : 2},
+        {a : 3}
+    ], function(err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+}
 
